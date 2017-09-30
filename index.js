052bd4af72d7139010944c9e5964e9c13a841d88
aldoso <alexdobras@gmail.com>
2017-10-01 02:29:35 +0300

    corrected

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// const express = require('express')
-//
-// //set up express app
-// const app = express()
-//
-// //listen for requests
-// //for deploying to online servers like heroky you need to have process.env.port not just a port like 4000
-// app.listen(process.env.port || 4000, function(){
-//   console.log('Connection established: now listening for requests')
-//   alert('it should work')
-// })
+const express = require('express')
+
+//set up express app
+const app = express()
+
+//listen for requests
+//for deploying to online servers like heroky you need to have process.env.port not just a port like 4000
+app.listen(process.env.port || 4000, function(){
+  console.log('Connection established: now listening for requests')
+  alert('it should work')
+})
 
 alert('Test 123');
- alert('it should work');
