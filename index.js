5f50cbcb6b261a4aa26800d66fded8224ea86f70
aldoso <alexdobras@gmail.com>
2017-10-01 20:33:11 +0300

    new123

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //requiring the express package
+const app = express(); //making the app use expressjs
 
+app.set('view engine', 'ejs') //setting the template engine to embeddedjs (ejs)
+
+app.listen(process.env.PORT || 3000) // creating the server (works with heroku)
+
+//Routes
 app.get("/", function(req, res){
   res.send('it works')
 })
 
+//display a static file
 app.get("/start", function(req, res){
   res.sendFile(__dirname + '/index.html')
 })
 
-app.get("/profile/:name", function(req, res){
+//display dynamic page
+app.get("/users/:name", function(req, res){
   res.send('You are wathing the profile of ' + req.params.name)
 })
-
-app.listen(process.env.PORT || 3000)
