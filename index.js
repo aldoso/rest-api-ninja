ebbf0fa6438eb6c6c166fa63920b45e50c6218ca
aldoso <alexdobras@gmail.com>
2017-10-01 15:45:04 +0300

    PORT not port

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello there, world!!!');
-}).listen(process.env.port || 8080);
+}).listen(process.env.PORT || 8080);
