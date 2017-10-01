85b632df6bf7865e6cb24b62a024dcf7b4e51f62
aldoso <alexdobras@gmail.com>
2017-10-01 17:46:48 +0300

    test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Capitalized Hello there, world!!!');
-}).listen(PROCESS.ENV.PORT || 8080);
+    res.end('Not Capitalized Hello there, world!!!');
+}).listen(process.env.PORT || 8080);
