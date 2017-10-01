e3df513fe677b6f5970caeb7f75fe3345a8c7b50
aldoso <alexdobras@gmail.com>
2017-10-01 17:40:53 +0300

    PORT UPPERCASE

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello there, world!!!');
-}).listen(process.env.PORT || 8080);
+}).listen(PROCESS.ENV.PORT || 8080);
