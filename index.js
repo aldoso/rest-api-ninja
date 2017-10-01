4107855bbc50cf0f58314170c975df1d3c57c927
aldoso <alexdobras@gmail.com>
2017-10-01 05:03:37 +0300

    sdfb

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.listen(process.env.PORT || 5000, function(req, respond) {
-  respond.send('worrkks')
+app.listen(process.env.PORT || 5000, function() {
   console.log('Node app is running on port')
 });
