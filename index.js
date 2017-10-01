5620c845904229ec032146288679ccea6bded370
aldoso <alexdobras@gmail.com>
2017-10-01 05:09:38 +0300

    sdefrgdsfg

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express()
 app.listen(process.env.PORT || 5000, function() {
   console.log('Node app is running on port')
 });
+
+
+app.get('/', function(req, res){
+  res.send('WWWorks')
+})
