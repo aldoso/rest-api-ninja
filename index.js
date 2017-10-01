7709249e1e78241c59bb6e5f8aeba077315f2e9b
aldoso <alexdobras@gmail.com>
2017-10-01 04:52:24 +0300

    sdfg

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.send('it should work now!!!')
-  console.log('bau!!!')
-});
+document.write('tadaaaam!!!')
+console.log ('this is the console')
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
