98099b4641a506714cb54045c7087794092db290
aldoso <alexdobras@gmail.com>
2017-10-01 15:34:37 +0300

    w3schools code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require('express')
-const app = express()
+var http = require('http');
 
-app.listen(process.env.PORT || 5000, function() {
-  console.log('Node app is running on port')
-});
-
-
-app.get('/', function(req, res){
-  res.send('WWWorks')
-})
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(process.env.port || 8080);
