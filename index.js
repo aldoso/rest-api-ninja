01fee73248dba9cbbddeaa75b8cfb155a736cfb1
aldoso <alexdobras@gmail.com>
2017-10-01 20:21:56 +0300

    12

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-app.listen(process.env.PORT || 1234)
+app.listen(process.env.PORT || 3000)
 
 app.get("/", function(req, res){
   res.send('it works')
+})
+
+app.get("/start", function(req, res){
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get("/profile/:name", function(req, res){
+  res.send('You are wathing the profile of ' + req.params.name)
 })
