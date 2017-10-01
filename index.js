c59b61085b24711e1deb504f1bba2788c05c7b35
aldoso <alexdobras@gmail.com>
2017-10-01 04:45:03 +0300

    2142

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('it works')
+  response.send('it works now!!!')
 });
 
 app.listen(app.get('port'), function() {
