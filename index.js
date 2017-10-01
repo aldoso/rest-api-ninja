e60ebc839543bac6117bd5ed301ad9849791b4ee
aldoso <alexdobras@gmail.com>
2017-10-01 04:59:52 +0300

    ss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.listen(process.env.PORT || 5000, function() {
-  document.write('tadaaaam!!!')
+app.listen(process.env.PORT || 5000, function(req, respond) {
+  respond.send('worrkks')
   console.log('Node app is running on port')
 });
