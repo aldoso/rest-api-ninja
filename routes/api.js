ff6b02eaa1ebdf2c07b9cf9b77475d57004df194
aldoso <alexdobras@gmail.com>
2017-10-21 23:53:53 +0300

    get request geolocation

--- routes/api.js
+++ routes/api.js
@@ -3,34 +3,52 @@ const router = express.Router()
 const Ninja = require('../models/ninja')
 
 //get a list of ninjas form the db
-router.get('/ninjas', function(req, res, next){
-  // res.send({type: 'GET'})
-  res.send('sdkfjsdkf')
+router.get('/ninjas', function(req, res, next) {
+  // Ninja.fing({}).then(function(ninjas) {
+  //   res.send(ninjas)
+  // })
+  Ninja.geoNear({
+    type: 'Point',
+    coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+  }, {
+    maxDistance: 100000,
+    spherical: true
+  }).then(function(ninjas) {
+    res.send(ninjas);
+  }).catch(next);
 })
 
 //add a new ninja to the db
-router.post('/ninjas', function(req, res, next){
+router.post('/ninjas', function(req, res, next) {
   // console.log(req.body)
-  Ninja.create(req.body).then(function(ninja){
+  Ninja.create(req.body).then(function(ninja) {
     res.send(ninja)
   }).catch(next)
 
 })
 
 //update a ninja in the db
-router.put('/ninjas/:id', function(req, res, next){
-  Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
-    Ninja.findOne({_id:req.params.id}).then(function(ninja){
-      res.send({type: 'PUT'})
+router.put('/ninjas/:id', function(req, res, next) {
+  Ninja.findByIdAndUpdate({
+    _id: req.params.id
+  }, req.body).then(function() {
+    Ninja.findOne({
+      _id: req.params.id
+    }).then(function(ninja) {
+      res.send({
+        type: 'PUT'
+      })
     })
   })
 })
 
 
 //delete a ninja form the db
-router.delete('/ninjas/:id', function(req, res, next){
+router.delete('/ninjas/:id', function(req, res, next) {
   // console.log(req.params.id)
-  Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja){
+  Ninja.findByIdAndRemove({
+    _id: req.params.id
+  }).then(function(ninja) {
     res.send(ninja)
   })
   // res.send({type: 'DELETE'})
